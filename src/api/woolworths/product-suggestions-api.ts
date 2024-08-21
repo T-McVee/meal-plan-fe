71abdf08b512e8fc5fe9caf9de274376efd6b5ae
tim mcvinish <tim.mcvinish@mojosoup.com.au>
2024-08-21 21:28:29 +1000

    Adds some mock api responses

---
+++ src/api/woolworths/product-suggestions-api.ts
@@ -0,0 +1,15 @@
+export const results = {
+  autoCorrectedTerm: "",
+  suggestions: [
+    "Tomato & BBQ Sauce",
+    "Tomato",
+    "Tomato Paste",
+    "Tomato Sauce",
+    "Tomato Truss",
+    "Tomatoes",
+    "Tomato Soup",
+    "Tomato Kumato",
+    "Tomato Loose Vine Ripened",
+    "Tomato Kumato Tomato Loose Kumato Loose",
+  ],
+};
